7d903ee9c9bc836dbe2560426ba3a7c9aafa921b
juxxon23 <juxxon23@hotmail.com>
2025-11-21 17:05:45 -0500

    Add script pause format

---
+++ pause_format.sh
@@ -0,0 +1,12 @@
+#! /bin/zsh/
+
+# Ingresar nombre del archivo
+# Procesamiento:
+# (1) Eliminar puntos suspensivos
+# (2) Reemplazar: 
+# [Coma (,) -> Tres (3) puntos (...)]
+# [Punto y coma (;) -> Cuatro (4) puntos (....)]
+# [Punto (.) -> Cinco (5) puntos (.....)]
+# [Dos Puntos (:) -> Cinco (5) puntos (.....)]
+# [Final de cita (".) -> Ocho (8) puntos (........)]
+# (3) Guardar el archivo formateado
