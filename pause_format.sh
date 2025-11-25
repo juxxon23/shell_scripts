f057bfdcfa52d091bda6559b99fe52fee0b588ea
juxxon23 <juxxon23@hotmail.com>
2025-11-25 18:16:48 -0500

    Update pause format script complete

--- pause_format.sh
+++ pause_format.sh
@@ -1,12 +1,23 @@
-#! /bin/zsh/
+#! /bin/zsh
 
-# Ingresar nombre del archivo
-# Procesamiento:
-# (1) Eliminar puntos suspensivos
-# (2) Reemplazar: 
-# [Coma (,) -> Tres (3) puntos (...)]
-# [Punto y coma (;) -> Cuatro (4) puntos (....)]
+# Initial vars
+echo "Enter the file name"
+read file_name
+cp $file_name file_name_cp 
+
+# Processing
+# Eliminar puntos suspensivos (opc)
+sed -r 's/\.{3}/./g' $file_name > file_name_cp
 # [Punto (.) -> Cinco (5) puntos (.....)]
 # [Dos Puntos (:) -> Cinco (5) puntos (.....)]
-# [Final de cita (".) -> Ocho (8) puntos (........)]
-# (3) Guardar el archivo formateado
+formatting=$(sed -r 's/[.:]/...../g' file_name_cp)
+# [Coma (,) -> Tres (3) puntos (...)]
+# [Linea (—) -> Tres (3) puntos (...)]
+formatting=$(sed -r 's/[,—]/.../g' <<< $formatting)
+# [Punto y coma (;) -> Cuatro (4) puntos (....)]
+formatting=$(sed -r 's/;/..../g' <<< $formatting)
+# [Final de cita (.....”) -> Ocho (8) puntos (........)]
+formatting=$(sed -r 's/\.{5}”/”......../g' <<< $formatting)
+
+# Save format file
+echo $formatting > format_result
